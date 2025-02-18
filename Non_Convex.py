17b1d66402e5b05cc05d1fbf762fe7358d1a4f23
Mindy_Ma <152960571+MindyBaby@users.noreply.github.com>
2025-02-18 18:39:12 +0800

    Update Non_Convex.py

--- Non_Convex.py
+++ Non_Convex.py
@@ -1,29 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import torch
 import numpy as np
 import torch.nn as nn
 from timeit import default_timer as timer
-
 import warnings
 warnings.filterwarnings("ignore")
 
-
-# In[2]:
-
-
 if torch.cuda.is_available():
     USE_CUDA = True  
-
 print('USE_CUDA = {}'.format(USE_CUDA))
-
-
-# In[3]:
-
 
 def f(x):
     if USE_CUDA:
@@ -33,22 +17,7 @@ def f(x):
     loss = loss_1 + loss_2
     return loss
 
-
-# In[4]:
-
-
-'''def f(x):
-    if USE_CUDA:
-        x = x.cuda()
-    loss = (x - 1)**2 + 100 * x**4  # 一维 Rosenbrock 函数
-    return loss.sum()'''
-
-
-# ## 2.构造LSTM_BlackBox优化器----2016
-
-# In[5]:
-
-
+# ## 1.LSTM_BlackBox----2016
 class LSTM_BlackBox_Optimizee_Model(nn.Module):   
     def __init__(self,input_size, output_size, hidden_size, num_stacks, batchsize, preprocess = True ,p = 10 ,output_scale = 1):
         super().__init__()
@@ -106,11 +75,6 @@ class LSTM_BlackBox_Optimizee_Model(nn.Module):
         #x = x + update
         x = torch.add(x, update)
         return x , next_state
-
-
-# In[6]:
-
-
 DIM = 2
 batchsize = 32
 Layers = 2
@@ -118,11 +82,6 @@ Hidden_nums = 20
 Input_DIM = DIM
 Output_DIM = DIM
 output_scale_value=1
-
-
-# In[7]:
-
-
 LSTM_BlackBox_Optimizee = LSTM_BlackBox_Optimizee_Model( Input_DIM, Output_DIM, Hidden_nums ,Layers , batchsize=batchsize,
                 preprocess=False,output_scale=output_scale_value)   ###### preprocess=False
 
@@ -131,12 +90,7 @@ print(LSTM_BlackBox_Optimizee)
 if USE_CUDA:
     LSTM_BlackBox_Optimizee = LSTM_BlackBox_Optimizee.cuda()
 
-
-# ## 2.构造Lr_LSTM_BlackBox优化器
-
-# In[8]:
-
-
+# ## 2.Lr_LSTM_BlackBox——Our model
 class LSTM_BlackBox_Optimizee_Model_lr(nn.Module):   
     def __init__(self,input_size, output_size, hidden_size, num_stacks, batchsize, preprocess = True ,p = 10 ,output_scale = 1):
         super().__init__()
@@ -198,11 +152,6 @@ class LSTM_BlackBox_Optimizee_Model_lr(nn.Module):
         #x = x + update
         x = torch.add(x, update)
         return x , next_state
-
-
-# In[9]:
-
-
 DIM = 2
 batchsize = 32
 Layers = 2
@@ -210,25 +159,14 @@ Hidden_nums = 20
 Input_DIM = DIM
 Output_DIM = DIM
 output_scale_value=1
-
-
-# In[10]:
-
-
 LSTM_BlackBox_Optimizee_lr = LSTM_BlackBox_Optimizee_Model_lr( Input_DIM, Output_DIM, Hidden_nums ,Layers , batchsize=batchsize,
                 preprocess=False,output_scale=output_scale_value)   ###### preprocess=False
 
 print(LSTM_BlackBox_Optimizee_lr)
-
 if USE_CUDA:
     LSTM_BlackBox_Optimizee_lr = LSTM_BlackBox_Optimizee_lr.cuda()
 
-
-# ## 7.优化问题目标函数的学习过程
-
-# In[11]:
-
-
+# ## 3.优化问题目标函数的学习过程
 class Learner( object ):
     def __init__(self,    
                  f ,  
@@ -378,72 +316,8 @@ class Learner( object ):
                 self.losses.append(loss.detach_())
                 
             return self.losses, self.global_loss_graph
-
-
-# In[12]:
-
-
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-
-STEPS = 100
-x = np.arange(STEPS)
-
-Adam = 'Adam' #因为这里Adam使用Pytorch
-SGD = 'SGD'
-RMS = 'RMS'
-AdaGrad = 'AdaGrad'
-
-#for _ in range(1): 
-for loop_count in range(1):  # 在这里设置循环次数
-   
-    SGD_Learner = Learner(f , SGD, STEPS, eval_flag=True,reset_theta=True,)
-    RMS_Learner = Learner(f , RMS, STEPS, eval_flag=True,reset_theta=True,)
-    Adam_Learner = Learner(f ,  Adam, STEPS, eval_flag=True,reset_theta=True,)
-    Adagrad_Learner = Learner(f,  AdaGrad, STEPS, eval_flag=True, reset_theta=True,)
-    LSTM_BlackBox_learner = Learner(f , LSTM_BlackBox_Optimizee, STEPS, eval_flag=True,reset_theta=True,retain_graph_flag=True)
-    LSTM_BlackBox_lr_learner = Learner(f , LSTM_BlackBox_Optimizee_lr, STEPS, eval_flag=True,reset_theta=True,retain_graph_flag=True)
-    
-
-    sgd_losses, sgd_sum_loss = SGD_Learner()
-    rms_losses, rms_sum_loss = RMS_Learner()
-    adam_losses, adam_sum_loss = Adam_Learner()
-    adagrad_losses, adagrad_sum_loss = Adagrad_Learner()
-    lstm_blackbox_losses, lstm_blackbox_sum_loss = LSTM_BlackBox_learner()
-    lstm_blackbox_lr_losses, lstm_blackbox_lr_sum_loss = LSTM_BlackBox_lr_learner()
-
-    
-    sgd_losses_tensor = torch.tensor(sgd_losses)
-    rms_losses_tensor = torch.tensor(rms_losses)
-    adam_losses_tensor = torch.tensor(adam_losses)
-    adagrad_losses_tensor = torch.tensor(adagrad_losses)
-    lstm_blackbox_losses_tensor = torch.tensor(lstm_blackbox_losses)
-    lstm_blackbox_lr_losses_tensor = torch.tensor(lstm_blackbox_lr_losses)
-    
-
-    p1, = plt.plot(x, sgd_losses_tensor.numpy(), label='SGD')
-    p2, = plt.plot(x, rms_losses_tensor.numpy(), label='RMS')
-    p3, = plt.plot(x, adam_losses_tensor.numpy(), label='Adam')
-    p4, = plt.plot(x, adagrad_losses_tensor.numpy(), label='AdaGrad')
-    p5, = plt.plot(x, lstm_blackbox_losses_tensor.numpy(), label='LSTM_BlackBox')
-    p6, = plt.plot(x, lstm_blackbox_lr_losses_tensor.numpy(), label='LSTM_BlackBox_Lr')
-
-    
-    plt.yscale('log')
-    plt.legend(handles=[p1, p2, p3, p4, p5, p6])
-    plt.title('Losses')
-    plt.show()
-    print("sum_loss:sgd={},rms={},adam={},adagrad={},lstm_black={},lstm_black_lr={}".format(sgd_sum_loss,rms_sum_loss,adam_sum_loss,
-                                                                                            adagrad_sum_loss,
-                                                                                            lstm_blackbox_sum_loss,lstm_blackbox_lr_sum_loss  ))
-
-
-# ## 9.自动学习的LSTM优化器Learning to learn
-
-# In[13]:
-
-
+            
+# ## 4.自动学习的LSTM优化器Learning to learn
 from timeit import default_timer as timer
 def Learning_to_learn_global_training(optimizee, global_taining_steps, Optimizee_Train_Steps, UnRoll_STEPS, 
                                       Evaluate_period ,optimizer_lr=0.1):
@@ -484,11 +358,7 @@ def Learning_to_learn_global_training(optimizee, global_taining_steps, Optimizee
     return global_loss_list, best_flag
 
 
-# ## 2.构造LSTM_BlackBox优化器----2016
-
-# In[14]:
-
-
+# ## 5.LSTM_BlackBox训练----2016
 def evaluate(best_sum_loss, best_final_loss, best_flag, lr):
     print('evalute the model(评估模型)')
     STEPS = 100
@@ -515,12 +385,8 @@ def evaluate(best_sum_loss, best_final_loss, best_flag, lr):
         
     return best_sum_loss, best_final_loss, best_flag
 
-
-# In[15]:
-
-
-Global_Train_Steps = 100 #可修改1000
-Optimizee_Train_Steps = 100#######100
+Global_Train_Steps = 100 
+Optimizee_Train_Steps = 100
 UnRoll_STEPS = 20
 Evaluate_period = 1 #可修改
 optimizer_lr = 0.1 #可修改
@@ -531,12 +397,7 @@ global_loss_list ,flag = Learning_to_learn_global_training( LSTM_BlackBox_Optimi
                                                             Evaluate_period,
                                                             optimizer_lr)
 
-
-# ## 2.构造Lr_LSTM_BlackBox优化器
-
-# In[16]:
-
-
+# ## 6.构造Lr_LSTM_BlackBox优化器
 def evaluate(best_sum_loss, best_final_loss, best_flag, lr):
     print('evalute the model(评估模型)')
     STEPS = 100
@@ -563,12 +424,8 @@ def evaluate(best_sum_loss, best_final_loss, best_flag, lr):
         
     return best_sum_loss, best_final_loss, best_flag
 
-
-# In[17]:
-
-
-Global_Train_Steps = 100 #可修改1000
-Optimizee_Train_Steps = 100#######100
+Global_Train_Steps = 100 
+Optimizee_Train_Steps = 100
 UnRoll_STEPS = 20
 Evaluate_period = 1 #可修改
 optimizer_lr = 0.1 #可修改
@@ -578,250 +435,3 @@ global_loss_list ,flag = Learning_to_learn_global_training( LSTM_BlackBox_Optimi
                                                             UnRoll_STEPS,
                                                             Evaluate_period,
                                                             optimizer_lr)
-
-
-# In[18]:
-
-
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-
-STEPS = 100
-x = np.arange(STEPS)
-
-Adam = 'Adam' #因为这里Adam使用Pytorch
-SGD = 'SGD'
-RMS = 'RMS'
-AdaGrad = 'AdaGrad'
-
-#for _ in range(1): 
-for loop_count in range(1):  # 在这里设置循环次数
-   
-    SGD_Learner = Learner(f , SGD, STEPS, eval_flag=True,reset_theta=True,)
-    RMS_Learner = Learner(f , RMS, STEPS, eval_flag=True,reset_theta=True,)
-    Adam_Learner = Learner(f ,  Adam, STEPS, eval_flag=True,reset_theta=True,)
-    Adagrad_Learner = Learner(f,  AdaGrad, STEPS, eval_flag=True, reset_theta=True,)
-    LSTM_BlackBox_learner = Learner(f , LSTM_BlackBox_Optimizee, STEPS, eval_flag=True,reset_theta=True,retain_graph_flag=True)
-    LSTM_BlackBox_lr_learner = Learner(f , LSTM_BlackBox_Optimizee_lr, STEPS, eval_flag=True,reset_theta=True,retain_graph_flag=True)
-    
-
-    sgd_losses, sgd_sum_loss = SGD_Learner()
-    rms_losses, rms_sum_loss = RMS_Learner()
-    adam_losses, adam_sum_loss = Adam_Learner()
-    adagrad_losses, adagrad_sum_loss = Adagrad_Learner()
-    lstm_blackbox_losses, lstm_blackbox_sum_loss = LSTM_BlackBox_learner()
-    lstm_blackbox_lr_losses, lstm_blackbox_lr_sum_loss = LSTM_BlackBox_lr_learner()
-
-    
-    sgd_losses_tensor = torch.tensor(sgd_losses)
-    rms_losses_tensor = torch.tensor(rms_losses)
-    adam_losses_tensor = torch.tensor(adam_losses)
-    adagrad_losses_tensor = torch.tensor(adagrad_losses)
-    lstm_blackbox_losses_tensor = torch.tensor(lstm_blackbox_losses)
-    lstm_blackbox_lr_losses_tensor = torch.tensor(lstm_blackbox_lr_losses)
-    
-
-    p1, = plt.plot(x, sgd_losses_tensor.numpy(), label='SGD')
-    p2, = plt.plot(x, rms_losses_tensor.numpy(), label='RMS')
-    p3, = plt.plot(x, adam_losses_tensor.numpy(), label='Adam')
-    p4, = plt.plot(x, adagrad_losses_tensor.numpy(), label='AdaGrad')
-    p5, = plt.plot(x, lstm_blackbox_losses_tensor.numpy(), label='LSTM_BlackBox')
-    p6, = plt.plot(x, lstm_blackbox_lr_losses_tensor.numpy(), label='LSTM_BlackBox_Lr')
-
-    
-    plt.yscale('log')
-    plt.legend(handles=[p1, p2, p3, p4, p5, p6])
-    plt.title('Losses')
-    plt.show()
-    print("sum_loss:sgd={},rms={},adam={},adagrad={},lstm_black={},lstm_black_lr={}".format(sgd_sum_loss,rms_sum_loss,adam_sum_loss,
-                                                                                            adagrad_sum_loss,
-                                                                                            lstm_blackbox_sum_loss,lstm_blackbox_lr_sum_loss  ))
-
-
-# In[19]:
-
-
-import matplotlib.pyplot as plt
-import numpy as np
-import torch
-
-# 定义颜色列表
-colors = ['blue', 'green', 'red', 'purple', 'orange', 'brown']
-
-# 定义线条样式列表
-linestyles = ['-', '--', '-.', ':', '-', '--']
-
-# 定义标记样式列表
-markers = ['*', 'o', 's', '^', 'D', 'x']
-
-# Monte Carlo实验函数
-def monte_carlo_experiment(num_experiments, num_steps, optimizee):
-    all_losses = []
-    
-    for _ in range(num_experiments):
-    
-        if optimizee == 'SGD':
-            learner = Learner(f,optimizee, num_steps, eval_flag=True, reset_theta=True)
-         
-        elif optimizee == 'RMS':
-            learner = Learner(f, optimizee, num_steps, eval_flag=True, reset_theta=True)
-            
-        elif optimizee == 'Adam':
-            learner = Learner(f, optimizee, num_steps, eval_flag=True, reset_theta=True)
-        
-        elif optimizee == 'AdaGrad':
-            learner = Learner(f, optimizee, num_steps, eval_flag=True, reset_theta=True)
-        
-        elif optimizee == LSTM_BlackBox_Optimizee:
-            learner = Learner(f, optimizee, num_steps, eval_flag=True, reset_theta=True, retain_graph_flag=True)
-            
-        elif optimizee == LSTM_BlackBox_Optimizee_lr:
-            learner = Learner(f, optimizee, num_steps, eval_flag=True, reset_theta=True, retain_graph_flag=True)
-            
-        losses, _ = learner()
-        losses_tensor = torch.tensor(losses)
-        all_losses.append(losses_tensor)        
-    
-    # 计算所有实验的平均损失、标准差和95%置信区间
-    all_losses_array = np.array(all_losses)
-    avg_losses = np.mean(all_losses_array, axis=0)
-    std_losses = np.std(all_losses_array, axis=0)
-    sem_losses = std_losses / np.sqrt(num_experiments)  # 标准误差
-    conf_interval = 1.96 * sem_losses  # 95%置信区间
-    return avg_losses, conf_interval
-
-# 设置参数
-num_experiments = 1  # 蒙特卡洛实验次数
-num_steps = 100
-
-# 进行蒙特卡洛实验
-avg_losses_sgd, conf_interval_sgd = monte_carlo_experiment(num_experiments, num_steps, 'SGD')
-avg_losses_rms, conf_interval_rms = monte_carlo_experiment(num_experiments, num_steps, 'RMS')
-avg_losses_adam, conf_interval_adam = monte_carlo_experiment(num_experiments, num_steps, 'Adam')
-avg_losses_adagrad, conf_interval_adagrad = monte_carlo_experiment(num_experiments, num_steps, 'AdaGrad')
-avg_losses_lstm_blackbox, conf_interval_lstm_blackbox = monte_carlo_experiment(num_experiments, num_steps, LSTM_BlackBox_Optimizee)
-avg_losses_lstm_blackbox_lr, conf_interval_lstm_blackbox_lr = monte_carlo_experiment(num_experiments, num_steps, LSTM_BlackBox_Optimizee_lr)
-
-# 绘制结果
-x = np.arange(num_steps)
-
-plt.figure(figsize=(10, 6))
-for i, (avg_losses, conf_interval, label) in enumerate([
-    (avg_losses_sgd, conf_interval_sgd, 'MBGD'),
-    (avg_losses_adagrad, conf_interval_adagrad, 'AdaGrad'),
-    (avg_losses_rms, conf_interval_rms, 'RMSprop'),
-    (avg_losses_adam, conf_interval_adam, 'Adam'),
-    (avg_losses_lstm_blackbox, conf_interval_lstm_blackbox, 'LSTM-DM'),
-    (avg_losses_lstm_blackbox_lr, conf_interval_lstm_blackbox_lr, 'LSTM-LR')
-]):
-    plt.plot(x, avg_losses, label=label, linestyle=linestyles[i % len(linestyles)], 
-             marker=markers[i % len(markers)], markersize=6, color=colors[i % len(colors)], 
-             markeredgewidth=0, markevery=10)  # 去掉标记边框并减少标记密度
-    plt.fill_between(x, avg_losses - conf_interval, avg_losses + conf_interval, color=colors[i % len(colors)], alpha=0.2)
-
-plt.yscale('log')
-plt.title('Rosenbrock Function Loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-# 添加图例并将其放在右上角
-plt.legend(loc='upper right')
-plt.show()
-
-
-# In[20]:
-
-
-import matplotlib.pyplot as plt
-import numpy as np
-import torch
-
-# 定义颜色列表
-colors = ['blue', 'green', 'red', 'purple', 'orange', 'brown']
-
-# 定义线条样式列表
-linestyles = ['-', '--', '-.', ':', '-', '--']
-
-# 定义标记样式列表
-markers = ['*', 'o', 's', '^', 'D', 'x']
-
-# Monte Carlo实验函数
-def monte_carlo_experiment(num_experiments, num_steps, optimizee):
-    all_losses = []
-    
-    for _ in range(num_experiments):
-    
-        if optimizee == 'SGD':
-            learner = Learner(f,optimizee, num_steps, eval_flag=True, reset_theta=True)
-         
-        elif optimizee == 'RMS':
-            learner = Learner(f, optimizee, num_steps, eval_flag=True, reset_theta=True)
-            
-        elif optimizee == 'Adam':
-            learner = Learner(f, optimizee, num_steps, eval_flag=True, reset_theta=True)
-        
-        elif optimizee == 'AdaGrad':
-            learner = Learner(f, optimizee, num_steps, eval_flag=True, reset_theta=True)
-        
-        elif optimizee == LSTM_BlackBox_Optimizee:
-            learner = Learner(f, optimizee, num_steps, eval_flag=True, reset_theta=True, retain_graph_flag=True)
-            
-        elif optimizee == LSTM_BlackBox_Optimizee_lr:
-            learner = Learner(f, optimizee, num_steps, eval_flag=True, reset_theta=True, retain_graph_flag=True)
-            
-        losses, _ = learner()
-        losses_tensor = torch.tensor(losses)
-        all_losses.append(losses_tensor)        
-    
-    # 计算所有实验的平均损失、标准差和95%置信区间
-    all_losses_array = np.array(all_losses)
-    avg_losses = np.mean(all_losses_array, axis=0)
-    std_losses = np.std(all_losses_array, axis=0)
-    sem_losses = std_losses / np.sqrt(num_experiments)  # 标准误差
-    conf_interval = 1.96 * sem_losses  # 95%置信区间
-    return avg_losses, conf_interval
-
-# 设置参数
-num_experiments = 1  # 蒙特卡洛实验次数
-num_steps = 100
-
-# 进行蒙特卡洛实验
-avg_losses_sgd, conf_interval_sgd = monte_carlo_experiment(num_experiments, num_steps, 'SGD')
-avg_losses_rms, conf_interval_rms = monte_carlo_experiment(num_experiments, num_steps, 'RMS')
-avg_losses_adam, conf_interval_adam = monte_carlo_experiment(num_experiments, num_steps, 'Adam')
-avg_losses_adagrad, conf_interval_adagrad = monte_carlo_experiment(num_experiments, num_steps, 'AdaGrad')
-avg_losses_lstm_blackbox, conf_interval_lstm_blackbox = monte_carlo_experiment(num_experiments, num_steps, LSTM_BlackBox_Optimizee)
-avg_losses_lstm_blackbox_lr, conf_interval_lstm_blackbox_lr = monte_carlo_experiment(num_experiments, num_steps, LSTM_BlackBox_Optimizee_lr)
-
-# 绘制结果
-x = np.arange(num_steps)
-
-plt.figure(figsize=(10, 6))
-for i, (avg_losses, conf_interval, label) in enumerate([
-    (avg_losses_sgd, conf_interval_sgd, 'MBGD'),
-    (avg_losses_adagrad, conf_interval_adagrad, 'AdaGrad'),
-    (avg_losses_rms, conf_interval_rms, 'RMSprop'),
-    (avg_losses_adam, conf_interval_adam, 'Adam'),
-    (avg_losses_lstm_blackbox, conf_interval_lstm_blackbox, 'LSTM-DM'),
-    (avg_losses_lstm_blackbox_lr, conf_interval_lstm_blackbox_lr, 'LSTM-LR')
-]):
-    plt.plot(x, avg_losses, label=label, linestyle=linestyles[i % len(linestyles)], 
-             marker=markers[i % len(markers)], markersize=6, color=colors[i % len(colors)], 
-             markeredgewidth=0, markevery=10)  # 去掉标记边框并减少标记密度
-    plt.fill_between(x, avg_losses - conf_interval, avg_losses + conf_interval, color=colors[i % len(colors)], alpha=0.2)
-
-plt.yscale('log')
-plt.title('Rosenbrock Function Loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.grid(True)  # 添加网格线
-# 添加图例并将其放在右上角
-plt.legend(loc='upper right')
-plt.show()
-
-
-# In[ ]:
-
-
-
-
